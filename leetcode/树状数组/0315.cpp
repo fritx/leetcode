fc4192fdecefac8562b18f48848a238408fe92f7
Fritz Lin <uxfritz@163.com>
2020-08-02 22:19:58 +0800

    misc

--- leetcode/树状数组/0315.cpp
+++ leetcode/树状数组/0315.cpp
@@ -3,6 +3,34 @@
 // https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/solution/ji-suan-you-ce-xiao-yu-dang-qian-yuan-su-de-ge-s-7/
 
 
+// 2. by newhar
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int> b(nums.begin(), nums.end());
+        sort(b.begin(),b.end());
+        b.erase(unique(b.begin(),b.end()),b.end());
+
+        vector<int> res(nums.size()), sz(4*b.size(),0);
+
+        for(int i = (int)nums.size()-1; i >= 0; --i) {
+            int l = 0, r = (int)b.size()-1, node = 1;
+            while(l < r) {
+                sz[node]++;
+                int mid = (l+r+1) >> 1;
+                if(nums[i] >= b[mid]) {
+                    res[i] += sz[node<<1], node = (node<<1)|1, l = mid;
+                } else {
+                    node <<= 1, r = mid - 1;
+                }
+            }
+            sz[node]++;
+        }
+        return res;
+    }
+};
+
+
 // 1. 方法一：离散化树状数组
 // 执行用时： 32 ms , 在所有 C++ 提交中击败了 69.63% 的用户
 // 内存消耗： 10.3 MB , 在所有 C++ 提交中击败了 100.00% 的用户
